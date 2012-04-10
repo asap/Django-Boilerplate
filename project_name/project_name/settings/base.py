7d54289be31aef6f08f9ed25057411dd1f2500db
Andy Dirnberger <dirn@dirnonline.com>
2012-04-10 12:48:54 -0400

    Remove STATIC_ROOT from STATICFILES_DIRS
    This was leading to an `ImproperlyConfigured` exception being raised while using runserver. Not only does this stop the exception from being raised, but now runserver is properly serving up static files, at least in the admin.

--- project_name/project_name/settings/base.py
+++ project_name/project_name/settings/base.py
@@ -21,9 +21,7 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
-]
+STATICFILES_DIRS = []
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
